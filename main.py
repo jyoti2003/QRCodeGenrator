545be4b05a8b9cb0eefcf650a9a5bb89e308d899
Soumya44 <soumyaranjanb02@gmail.com>
2020-10-04 20:39:06 +0530

    Added main.py

--- main.py
+++ main.py
@@ -11,4 +11,7 @@ s = "www.geeksforgeeks.org"
 url = pyqrcode.create(s) 
   
 # Create and save the svg file naming "myqr.svg" 
-url.svg("myqr.svg")
+url.svg("myqr.svg", scale = 8) 
+  
+# Create and save the png file naming "myqr.png" 
+url.png('myqr.png', scale = 6) 
